be5c60caa7baed5b07d5be319dfa877d6f1a3252
sho-choma-tech <61417295+sho-choma-tech@users.noreply.github.com>
2021-05-31 09:08:05 +0900

    Add files via upload

---
+++ ch1/Python/list1_2.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import numpy.matlib
+import pandas as pd 
+import warnings
+warnings.filterwarnings('ignore')
+
+
+
+color_ = np.array([[0,0,0,1,1,1], [0,1,0,1,0,1]])
+card_ = np.random.multinomial(n=10000, size=1, pvals=[1/6, 1/6, 1/6, 1/6, 1/6, 1/6])
+r_ = np.sum(card_*color_[0])
+rr_ = np.sum(card_*color_[0]*color_[1])
+ans = rr_ / r_
+print(ans)
